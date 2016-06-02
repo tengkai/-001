7616e89ddaa6a4c506d1fa3ebdc25d98659f2530
tengkai <1905245024@qq.com>
2016-06-02 09:16:06 +0800

    Initial Commit

---
+++ 小厨房01/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  小厨房01
+//
+//  Created by BEVER on 16/6/2.
+//  Copyright © 2016年 BEVER. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 小厨房01/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  小厨房01
+//
+//  Created by BEVER on 16/6/2.
+//  Copyright © 2016年 BEVER. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
