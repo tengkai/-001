f647f4910ef78c5aabee9cfab536155c60a2ad25
tengkai <1905245024@qq.com>
2016-06-02 09:53:25 +0800

    111
    dddd

--- 小厨房01/ViewController.m
+++ 小厨房01/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    //是打工奋斗的的顶顶顶顶顶顶顶顶顶顶
 }
 
 - (void)didReceiveMemoryWarning {
